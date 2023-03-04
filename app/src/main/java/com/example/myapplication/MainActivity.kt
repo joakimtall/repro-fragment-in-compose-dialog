f8513c65ba31ea9c81f350c0187dc36df668f4cd
Joakim Tall <joakim.tall@bontouch.com>
2023-03-04 11:57:47 +0100

    Rework case to show back handler order

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import androidx.activity.OnBackPressedDispatcher
 import androidx.activity.compose.BackHandler
+import androidx.activity.compose.LocalOnBackPressedDispatcherOwner
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.background
@@ -18,27 +20,52 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.viewinterop.AndroidViewBinding
 import com.example.myapplication.databinding.FragmentContainerBinding
 import com.example.myapplication.ui.theme.MyApplicationTheme
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.isActive
+import kotlin.reflect.full.memberProperties
+import kotlin.reflect.jvm.isAccessible
+import kotlin.reflect.jvm.jvmName
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            var count by remember { mutableStateOf(0) }
+            var backHandlerActive by remember { mutableStateOf(true) }
+            BackHandler(backHandlerActive) {
+                count += 1
+            }
             MyApplicationTheme {
                 Column(modifier = Modifier.fillMaxSize()) {
-                    Column(modifier = Modifier.fillMaxWidth().weight(0.3f).background(Color.White)) {
-                        var count by remember { mutableStateOf(0) }
-                        var backHandlerActive by remember { mutableStateOf(true) }
-                        BackHandler(backHandlerActive) {
-                            count += 1
-                        }
-                        Text("In root compose world (outside fragment)\nBack pressed count: $count")
+                    Column(modifier = Modifier
+                        .fillMaxWidth()
+                        .weight(0.5f)
+                        .background(Color.White)) {
+                        Text("In compose world (outside fragment)\nBack pressed count: $count")
                         Button(onClick = { backHandlerActive = !backHandlerActive }) {
                             Text("Toggle outside fragment back handler active ($backHandlerActive)")
                         }
+                        var text by remember { mutableStateOf("") }
+                        val backPressedDispatcherOwner = LocalOnBackPressedDispatcherOwner.current
+                        LaunchedEffect(Unit) {
+                            while (isActive) {
+                                text = backPressedDispatcherOwner?.let { owner ->
+                                    owner.onBackPressedDispatcher.getPrivateProperty<OnBackPressedDispatcher, java.util.ArrayDeque<*>>(
+                                        "mOnBackPressedCallbacks"
+                                    )?.let { backPressedCallbacks ->
+                                       backPressedCallbacks.joinToString("\n") {
+                                           it::class.jvmName
+                                       }
+                                    }
+                                } ?: "got null"
+                                delay(200)
+                            }
+                        }
+                        Text("mOnBackPressedCallbacks: \n$text")
                     }
                     AndroidViewBinding(
                         factory = FragmentContainerBinding::inflate,
-                        modifier = Modifier.weight(0.7f),
+                        modifier = Modifier.weight(0.5f),
                     )
                 }
             }
@@ -46,15 +73,9 @@ class MainActivity : AppCompatActivity() {
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    MyApplicationTheme {
-        Greeting("Android")
-    }
-}
+inline fun <reified T : Any, R> T.getPrivateProperty(name: String): R? =
+    T::class
+        .memberProperties
+        .firstOrNull { it.name == name }
+        ?.apply { isAccessible = true }
+        ?.get(this) as? R
